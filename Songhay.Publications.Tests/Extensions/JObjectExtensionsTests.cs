ec45968c2c90ed459247220e4ae33d94ff5fcc6e
Bryan Wilhite <bryan.wilhite@gmail.com>
2020-07-09 18:01:10 -0700

    #15 added `GetFindChangeArgs_Test` :microscope:

--- Songhay.Publications.Tests/Extensions/JObjectExtensionsTests.cs
+++ Songhay.Publications.Tests/Extensions/JObjectExtensionsTests.cs
@@ -1,15 +1,21 @@
 ﻿using Newtonsoft.Json.Linq;
+using Songhay.Extensions;
+using Songhay.Models;
+using Songhay.Publications.Activities;
+using Songhay.Publications.Extensions;
 using System.IO;
 using Xunit;
-using Songhay.Publications.Extensions;
-using Songhay.Models;
-using Songhay.Extensions;
-using Songhay.Publications.Activities;
+using Xunit.Abstractions;
 
 namespace Songhay.Publications.Tests.Extensions
 {
     public class JObjectExtensionsTests
     {
+        public JObjectExtensionsTests(ITestOutputHelper helper)
+        {
+            this._testOutputHelper = helper;
+        }
+
         [Theory]
         [InlineData("md-add-entry-extract-settings.json", "../../../markdown/shell")]
         public void GetAddEntryExtractArg_Test(string settingsFile, string presentationRoot)
@@ -95,6 +101,42 @@ namespace Songhay.Publications.Tests.Extensions
         }
 
         [Theory]
+        [InlineData("md-find-change-settings.json", "../../../markdown/shell")]
+        public void GetFindChangeArgs_Test(string settingsFile, string presentationRoot)
+        {
+            presentationRoot = FrameworkAssemblyUtility.GetPathFromAssembly(this.GetType().Assembly, presentationRoot);
+
+            var args = new ProgramArgs(new[]
+                {
+                    ProgramArgs.SettingsFile, settingsFile,
+                    ProgramArgs.BasePath, presentationRoot
+                });
+
+            var (presentationInfo, settingsInfo) = args.ToPresentationAndSettingsInfo();
+
+            Assert.True(presentationInfo.Exists);
+            Assert.True(settingsInfo.Exists);
+
+            var jO = JObject.Parse(File.ReadAllText(settingsInfo.FullName));
+            var (input, pattern, replacement, useRegex, outputPath) = jO.GetFindChangeArgs(presentationInfo);
+
+            Assert.False(string.IsNullOrEmpty(input));
+            Assert.False(string.IsNullOrEmpty(pattern));
+            Assert.False(string.IsNullOrEmpty(replacement));
+            Assert.True(useRegex);
+            Assert.False(string.IsNullOrEmpty(outputPath));
+
+            var inputPath = jO.GetValue<string>("inputPath");
+
+            this._testOutputHelper.WriteLine($"{nameof(inputPath)}: {inputPath}");
+            this._testOutputHelper.WriteLine($"{nameof(input)}: {input.Substring(0, 16)}...");
+            this._testOutputHelper.WriteLine($"{nameof(pattern)}: {pattern}");
+            this._testOutputHelper.WriteLine($"{nameof(replacement)}: {replacement}");
+            this._testOutputHelper.WriteLine($"{nameof(useRegex)}: {useRegex}");
+            this._testOutputHelper.WriteLine($"{nameof(outputPath)}: {outputPath}");
+        }
+
+        [Theory]
         [InlineData("md-generate-entry-settings.json", "../../../markdown/shell")]
         public void GetGenerateEntryArgs_Test(string settingsFile, string presentationRoot)
         {
@@ -149,5 +191,7 @@ namespace Songhay.Publications.Tests.Extensions
             var commandName = jO.GetPublicationCommand();
             Assert.Equal(nameof(MarkdownEntryActivity.PublishEntry), commandName);
         }
+
+        readonly ITestOutputHelper _testOutputHelper;
     }
 }
