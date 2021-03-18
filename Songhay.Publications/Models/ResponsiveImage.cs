f5c6d07c5f8de5542f72d0107dcf1fca893d34c5
Bryan Wilhite <rasx@songhaysystem.com>
2021-03-17 18:11:47 -0700

    resolves #41 resolves #42 🚜✨

---
+++ Songhay.Publications/Models/ResponsiveImage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Songhay.Publications.Models
+{
+    /// <summary>
+    /// Defines a responsive image
+    /// </summary>
+    public class ResponsiveImage
+    {
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key.
+        /// </summary>
+        /// <value>
+        /// The key.
+        /// </value>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Gets or sets the source.
+        /// </summary>
+        /// <value>
+        /// The source.
+        /// </value>
+        public Uri Source { get; set; }
+
+        /// <summary>
+        /// Gets or sets the candidates.
+        /// </summary>
+        /// <value>
+        /// The candidates.
+        /// </value>
+        public ICollection<ImageCandidate> Candidates { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sizes.
+        /// </summary>
+        /// <value>
+        /// The sizes.
+        /// </value>
+        public ICollection<ImageSize> Sizes { get; set; }
+    }
+}
