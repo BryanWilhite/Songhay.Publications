2088e1878ce9325bf1e1428f2e799e0453fbfe47
Bryan Wilhite <bryan.wilhite@gmail.com>
2022-07-23 15:33:16 -0700

    🚜✨ 🐚 statiq

---
+++ Statiq.Docs.Shell/Program.cs
@@ -0,0 +1,29 @@
+﻿using Statiq.Common;
+using Statiq.App;
+using Statiq.Docs;
+using Statiq.Web;
+
+await Bootstrapper
+    .Factory
+    .CreateDocs(args)
+    .AddExcludedPath("../../../../Statiq.Docs.Shell")
+    .AddSettings(new Dictionary<string, object>
+    {
+        { Keys.Title, "API" },
+        { Keys.LinkRoot, "Songhay.Publications" },
+        { DocsKeys.ApiPath, "latest" },
+        {
+            DocsKeys.SourceFiles,
+            new []
+            {
+                "../../../../../Songhay.Publications/**/{!.git,!bin,!obj,!packages,!*.Tests,}/**/*.cs",
+            }
+        },
+        { DocsKeys.OutputApiDocuments, true },
+        { WebKeys.Author, "@BryanWilhite" },
+        { WebKeys.Copyright, $"(c) {DateTime.Now.Year} Songhay System" },
+        { WebKeys.GitHubName, "Bryan Wilhite" },
+        { WebKeys.GitHubUsername, "BryanWilhite" },
+        { WebKeys.OutputPath, "../../../../docs" },
+    })
+    .RunAsync();
