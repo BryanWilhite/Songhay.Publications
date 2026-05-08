80ec9f531a5034bd41fc5769974c035d7f09d44e
Bryan Wilhite <bryan.wilhite@gmail.com>
2026-05-07 19:17:18 -0700

    add tests to answer the following questions:
    - Can I prove in a unit test that the `IDictionary<object, object>` from `IDeserializer.Deserialize` is or is not boxing strings?
    - Why did I not investigate the `IDeserializer.Deserialize<T>()` method(s)  where `T` is `Document`?

--- Songhay.Publications.Tests/YamlUtilityTests.cs
+++ Songhay.Publications.Tests/YamlUtilityTests.cs
@@ -1,3 +1,8 @@
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+using System.Text.Json.Nodes;
+
 namespace Songhay.Publications.Tests;
 
 public class YamlUtilityTests(ITestOutputHelper helper)
@@ -5,7 +10,7 @@ public class YamlUtilityTests(ITestOutputHelper helper)
     [Theory]
     [InlineData("""
 
-                scalar: a scalar
+                scalar: 42
                 sequence:
                   - one
                   - two
@@ -24,7 +29,8 @@ public class YamlUtilityTests(ITestOutputHelper helper)
     [Theory]
     [InlineData("""
 
-                myString: a scalar
+                myString: hello world
+                myNumber: 42
                 myBoolean: true
                 myDate: 2005-12-30T23:16:54.0000000
                 sequence:
@@ -35,12 +41,95 @@ public class YamlUtilityTests(ITestOutputHelper helper)
     public void ShouldDeserializeYamlAndSerializeToJson(string yaml)
     {
         helper.WriteLine(yaml);
-        helper.WriteLine("deserializing to `IDictionary<string, string?>`...");
+        helper.WriteLine("deserializing to `IDictionary<string, object>`...");
 
-        IDictionary<string, object>? yO = YamlUtility.DeserializeYaml(yaml);
+        IDictionary<string, object>? data = YamlUtility.DeserializeYaml(yaml);
 
-        Assert.NotNull(yO);
+        Assert.NotNull(data);
 
-        helper.WriteLine(yO.ToJsonString());
+        JsonObject jsonObject = new();
+
+        foreach (KeyValuePair<string, object> kvp in data)
+        {
+            helper.WriteLine(kvp.Value.GetType().Name);
+            jsonObject[kvp.Key] = JsonValue.Create(kvp.Value);
+        }
+
+        helper.WriteLine(jsonObject.ToJsonString());
+    }
+
+    [Theory]
+    [InlineData(
+        """
+            documentId: 9609
+            title: 'Michael A. Gonzales: 7 Questions from Invisible Woman'
+            documentShortName: 
+            fileName: kp_blackadelic.html
+            path: ./
+            templateId: 9059
+            segmentId: 4585
+            isRoot: true
+            isActive: true
+            sortOrdinal: 
+            clientId: 
+            endDate: 
+            inceptDate: 2008-03-28T13:38:00.0000000
+            modificationDate: 2011-07-13T12:18:38.3400000
+            """
+            )]
+    public void ShouldDeserializeYamlToDocument(string yaml)
+    {
+        IDeserializer deserializer = new DeserializerBuilder()
+            .IgnoreUnmatchedProperties()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .WithAttributeOverride(typeof(Document), nameof(Document.ClientId), new YamlIgnoreAttribute())
+            .Build();
+
+        Document actual = deserializer.Deserialize<Document>(yaml);
+
+        helper.WriteLine(actual.ToDisplayText());
+    }
+
+    // ReSharper disable once ClassNeverInstantiated.Local
+    private record OrderItem
+    {
+        // ReSharper disable once UnusedMember.Local
+        public string? PartNumber { get; set; }
+        // ReSharper disable once UnusedMember.Local
+        public string? Description { get; set; }
+        // ReSharper disable once UnusedMember.Local
+        public decimal? Price { get; set; }
+        // ReSharper disable once UnusedMember.Local
+        public int? Quantity { get; set; }
+    }
+
+    ///<remarks>
+    /// This test verifies that YamlDotNet:
+    /// - supports records
+    /// - supports non-public types
+    /// - supports properties with nullable types
+    /// - can be configured to ignore properties not on a specified type
+    ///</remarks>
+    [Theory]
+    [InlineData(
+        """
+        partNumber: WXY5527-INC
+        description: This is the widget of the other widget.
+        price: 59.99
+        quantity: 42
+        tags:
+        - one
+        - two
+        """)]
+    public void ShouldDeserializeYamlToOrderItem(string yaml)
+    {
+        IDeserializer deserializer = new DeserializerBuilder()
+            .IgnoreUnmatchedProperties()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .Build();
+
+        OrderItem actual = deserializer.Deserialize<OrderItem>(yaml);
+
+        helper.WriteLine($"{actual}");
     }
 }
